8ed4c28c86e2f2a4c6ea47ebf601f1b07cf6ebf2
holdonthecold <holdonthecold@gmail.com>
2025-11-20 06:24:09 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import ChatUI from "../components/ChatUI";
-
-export default function Page() {
-  return <ChatUI />;
-}
+await fetch("/api/chat", {
+  method: "POST",
+  body: JSON.stringify({
+    messages: chatMessages
+  })
+});
