090caf23e92e6bcc24222b4821f1c62688f5f6fb
holdonthecold <holdonthecold@gmail.com>
2025-11-20 05:03:48 +0800

    Create Home component with ChatUI integration

---
+++ pages/pages/index.tsx
@@ -0,0 +1,10 @@
+import ChatUI from "../components/ChatUI";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>An · Chat</h1>
+      <ChatUI />
+    </div>
+  );
+}
